dde39abf38f69876d1d16852bf892175e229fd6c
lenovo <nupin.pillai@metalstreet.in>
2020-12-02 17:36:40 +0530

    first commit

---
+++ src/components/StatusBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-layout align-center class="headline">
+        <span class="mx-3">Turns: {{turns}}</span>
+        <v-divider vertical></v-divider>
+        <span class="mx-3">Matches: {{matches}}</span>
+    </v-layout>
+</template>
+
+<script>
+    export default {
+        props: ["turns", "matches"]
+    }
+</script>
+
+<style scoped>
+
+</style>
